9ab69e3c247d9567119642afd9634bdcf7963890
xma <2316271258@qq.com>
2022-05-03 15:06:00 +0800

    add category page

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,10 +1,26 @@
 <template>
-
+<search-box @tap="handleSearchBoxClick"></search-box>32
+<foot-menu checked="4"></foot-menu>
 </template>
 
 <script>
+import FootMenu from '../basic/footMenu'
+import SearchBox from '../search/search-box/search-box.vue'
 export default {
-  name: "category"
+  name: "category",
+  components: {
+    FootMenu,
+    SearchBox
+  },
+  setup: function (props, context) {
+    console.log(props, context);
+    const handleSearchBoxClick = function () {
+      console.log(323);
+    }
+    return {
+      handleSearchBoxClick
+    }
+  }
 }
 </script>
 
